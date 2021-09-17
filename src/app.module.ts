6199be1ce46e82ed66db38d2dcb7d56c989df933
takumi-n <takumi.chillout@gmail.com>
2021-09-18 00:14:47 +0900

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,10 @@
-import { Cat } from './cats/entities/cat.entity';
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [
-    CatsModule,
-    CacheModule.register(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 5000,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [Cat],
-      synchronize: true,
-    }),
-  ],
+  imports: [CatsModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [],
 })
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,5 +1,5 @@
 import { Cat } from './entities/cat.entity';
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,6 +7,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
-  imports: [TypeOrmModule.forFeature([Cat])],
+  imports: [TypeOrmModule.forFeature([Cat]), CacheModule.register()],
 })
 export class CatsModule {}
